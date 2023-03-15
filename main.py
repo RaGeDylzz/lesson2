5935bb1e39e7d01a3f5b4ac6eaa878d84bccc403
Dylan McCarthy <dylansimon808@gmail.com>
2023-03-15 11:08:17 +0000

    added further question and reported number of correct answers

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-# Times tables etc
 import random
-
-# number = int(input("Enter a number from 1-12 (for loop): "))
-#
-# for i in range(1, 13):
-#     print(i, "x", number, "=", i * number)
-#
-# print("Finished for loop...")
-# print()
 
 numberEntered = int(input("Enter a number from 1-12(while loop): "))
 count = 1
@@ -21,12 +12,23 @@ print("Finished while loop...")
 for x in range(100):
     print()
 
-randomNumber = random.randint(1, 13)
-isIncorrect = True
-while isIncorrect:
-    answer = int(input("What is " + str(randomNumber) + " x " + str(numberEntered) + "? "))
-    if answer == randomNumber * numberEntered:
+
+def ask_question():
+    random_number = random.randint(1, 13)
+    answer_input = int(input("What is " + str(random_number) + " x " + str(numberEntered) + "? "))
+    if answer_input == random_number * numberEntered:
         print("You have gotten it right well done!")
-        isIncorrect = False
+        return True
     else:
-        print("No! Try again.")
+        print("Incorrect!")
+        return False
+
+
+correct_answers = 0
+for i in range(5):
+    if ask_question():
+        correct_answers = correct_answers + 1
+
+
+print("You got", correct_answers, "out of 5")
+
